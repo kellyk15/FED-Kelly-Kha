37da7a023f8711bc7315eb761ad65e8f50d894e2
kellyk15 <kellykha2004@gmail.com>
2024-09-16 12:28:26 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
-console.log("hi");
+var openButton = document.querySelector("header > button");
+// openButton.onclick = openMenu;
+
+openButton.addEventListener('click',openMenu)
+
+function openMenu (){
+    var deNav = document.querySelector("header nav");
+    deNav.classList.toggle("toonMenu");
+
+}
+
+// var sluitButton = document.querySelector("header > button");
+// sluitButton.onclick = sluitMenu;
+
+// function sluitMenu (){
+//     var deNav = document.querySelector("header nav");
+//     deNav.classList.remove("toonMenu");
+ 
+// }
